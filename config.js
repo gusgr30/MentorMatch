2bffe3c8d8a06d4c65225852824dbe70263da1d3
Julieta Rosales <38292854@comunidadort.edu.ar>
2026-06-04 16:46:36 -0300

    fix STRCNX

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const MODELO_PERSISTENCIA = process.env.MODELO_PERSISTENCIA || "";
-const STRCNX = process.env.STRCNX || "";
+const STRCNX = process.env.STRCNX || "mongodb://localhost:27017";
 const BASE = process.env.BASE || "test";
 
 export default {
