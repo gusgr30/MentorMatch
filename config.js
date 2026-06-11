19a324632eae5720694ddd71722e790d063d72e6
Julieta Rosales <38292854@comunidadort.edu.ar>
2026-06-11 17:16:36 -0300

    se agrega nueva const para JWT

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT || 8080;
 const MODELO_PERSISTENCIA = process.env.MODELO_PERSISTENCIA || "";
 const STRCNX = process.env.STRCNX || "mongodb://localhost:27017";
 const BASE = process.env.BASE || "test";
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 export default {
   PORT,
   MODELO_PERSISTENCIA,
   STRCNX,
   BASE,
+  JWT_SECRET,
 };
