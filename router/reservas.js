7dd83d1ba2c99e3c5805829dbc7f4b9d2379698b
Julieta Rosales <38292854@comunidadort.edu.ar>
2026-06-11 17:17:30 -0300

    se agrega middleware token en rutas de reservas

--- router/reservas.js
+++ router/reservas.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ReservaController from "../controller/reserva.js";
+import verificarToken from "../middleware/auth.js";
 
 class RouterReservas {
   #controlador;
@@ -11,11 +12,11 @@ class RouterReservas {
   config() {
     const router = express.Router();
 
-    router.get("/usuario/:userId", this.#controlador.obtenerReservasPorUsuario);
-    router.get("{/:id}", this.#controlador.obtenerReservas);
-    router.post("/", this.#controlador.guardarReserva);
-    router.put("/:id/cancelar", this.#controlador.cancelarReserva);
-    router.put("/:id/confirmar", this.#controlador.confirmarReserva);
+    router.get("/usuario/:userId", verificarToken, this.#controlador.obtenerReservasPorUsuario);
+    router.get("{/:id}", verificarToken, this.#controlador.obtenerReservas);
+    router.post("/", verificarToken, this.#controlador.guardarReserva);
+    router.put("/:id/cancelar", verificarToken, this.#controlador.cancelarReserva);
+    router.put("/:id/confirmar", verificarToken, this.#controlador.confirmarReserva);
     // router.delete("/:id", this.#controlador.borrarReserva);
 
     return router;
