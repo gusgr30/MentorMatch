0b6d696b9a8fe2c40e3d032be8f2521b1df439e1
Julieta Rosales <38292854@comunidadort.edu.ar>
2026-06-12 14:57:03 -0300

    se agrega metodo obtenerPorId  para la autenticacion

--- service/auth.js
+++ service/auth.js
@@ -1,48 +1,53 @@
-import UsuariosFactory from '../model/DAO/usuarios/usuariosFactory.js'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import config from '../config.js'
+import UsuariosFactory from "../model/DAO/usuarios/usuariosFactory.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config.js";
 
 class ServiceAuth {
-    #modelo
+  #modelo;
 
-    constructor(){
-        const modo = config.MODELO_PERSISTENCIA
-        this.#modelo = UsuariosFactory.get(modo)
+  constructor() {
+    const modo = config.MODELO_PERSISTENCIA;
+    this.#modelo = UsuariosFactory.get(modo);
+  }
+
+  obtenerPorId = async (id) => {
+    const usuario = await this.#modelo.obtenerUsuarioPorId(id);
+    if (!usuario) {
+      const error = new Error("Usuario no encontrado");
+      error.status = 404;
+      throw error;
+    }
+    const { email, nombre, rol, fotoUrl, mentorProfile } = usuario;
+    return { email, nombre, rol, fotoUrl, mentorProfile };
+  };
+
+  login = async (emailBody, passBody) => {
+    const usuario = await this.#modelo.obtenerUsuarioPorEmail(emailBody);
+    if (!usuario) {
+      const error = new Error("El email no se encuentra registrado");
+      error.status = 404;
+      throw error;
+    }
+    const { email, password, nombre, rol, fotoUrl, mentorProfile } = usuario;
+
+    const valido = await bcrypt.compare(passBody, password);
+
+    if (!valido) {
+      const error = new Error("Contraseña incorrecta");
+      error.status = 401;
+      throw error;
     }
 
-    login = async (emailBody, passBody) =>{
-        const usuario = await this.#modelo.obtenerUsuarioPorEmail(emailBody)
-        if(!usuario){
-            const error = new Error("El email no se encuentra registrado")
-            error.status = 404
-            throw error
-        }
-        const {email,password,nombre,rol,fotoUrl,mentorProfile} = usuario
-        
-        const valido = await bcrypt.compare(passBody, password)
+    const token = jwt.sign({ id: usuario._id, email, rol }, config.JWT_SECRET, {
+      expiresIn: "24h",
+    });
 
-        if(!valido){
-            const error =  new Error("Contraseña incorrecta")
-            error.status = 401
-            throw error
-        }
-
-        const token = jwt.sign(
-            { id: usuario._id, email, rol },
-            config.JWT_SECRET,
-            { expiresIn: '24h' }
-        )
-
-        return {
-            token,
-            email,
-            nombre,
-            rol,
-            fotoUrl,
-            mentorProfile
-        }
-    }
+    return {
+      token,
+      user: { email, nombre, rol, fotoUrl, mentorProfile },
+    };
+  };
 }
 
-export default ServiceAuth
+export default ServiceAuth;
