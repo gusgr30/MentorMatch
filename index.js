d5d2e7a48a843c4f13ddfdea52afd36bac404df7
Julieta Rosales <38292854@comunidadort.edu.ar>
2026-06-17 22:50:38 -0300

    se modifica el index para qe el servidor tenga el modelo de persistencia

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import config from "./config.js";
-import CnxMongoDB from "./model/DBMongo.js";
 import Server from "./server.js";
 import Mailer from "./service/email.js";
 
 await Mailer.config()
 
-if (config.MODELO_PERSISTENCIA === "MONGODB") {
-  await CnxMongoDB.conectar();
-}
-
-const server = new Server(config.PORT);
+const server = new Server(config.PORT, config.MODELO_PERSISTENCIA);
 server.start();
